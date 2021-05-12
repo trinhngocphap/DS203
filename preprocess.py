f0723c5efabf2e2b8fe00c2ffc1fb20013572b2f
trinhngocphap <phaptrinh2000@gmail.com>
2021-05-12 10:51:31 +0700

    update

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,6 @@
 import yaml
 from data_process.data_process import data_process
 
-config = yaml.safe_load(open('/content/MAMS-for-ABSA/config.yml'))
+config = yaml.safe_load(open('/content/DS203/config.yml'))
 
 data_process(config)
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import yaml
 import os
 from train.test import test
 
-config = yaml.safe_load(open('/content/MAMS-for-ABSA/config.yml'))
+config = yaml.safe_load(open('/content/DS203/config.yml'))
 mode = config['mode']
 os.environ["CUDA_VISIBLE_DEVICES"] = str(config['aspect_' + mode + '_model'][config['aspect_' + mode + '_model']['type']]['gpu'])
 test(config)
